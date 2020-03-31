b933469e049ce7d586dc8cca6aee22714641892a
sidneibiz <sidneibiz@gmail.com>
2020-03-30 23:49:25 -0300

    Ajustar build e lint do projeto para criar pipeline

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { CheckboxModule, IconsModule } from 'angular-bootstrap-md';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   imports: [],
@@ -8,6 +11,9 @@ import { CheckboxModule, IconsModule } from 'angular-bootstrap-md';
     SideBarComponent,
     CheckboxModule,
     IconsModule,
+    FormsModule,
+    MatSelectModule,
+    MatSidenavModule,
   ],
   providers: [],
   declarations: [SideBarComponent]
